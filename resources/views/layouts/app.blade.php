f2349bc0ddcbb41151ac9aab95c202fd200a4769
sanseru <sanseru1@yahoo.com>
2023-07-24 08:40:56 +0700

    Add more feature

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/css/main.css', 'resources/js/app.js', 'resources/js/main.js'])
+    @vite(['resources/css/app.css', 'resources/css/main.css', 'resources/js/app.js','resources/js/main.js'])
 </head>
 
 <body class="font-sans antialiased">
@@ -418,7 +418,8 @@
     <script src="https://code.jquery.com/jquery-3.7.0.min.js"
         integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
     <script src="https://kit.fontawesome.com/6b37673df3.js" crossorigin="anonymous"></script>
-    <script src="https://cdn.ckeditor.com/ckeditor5/38.1.0/classic/ckeditor.js"></script>
+    <script src="{{ asset('js/ckeditor.js') }}"></script>
+    {{-- <script src="https://cdn.ckeditor.com/ckeditor5/38.1.0/classic/ckeditor.js"></script> --}}
 
     {{-- <script src="https://cdn.ckeditor.com/ckeditor5/38.1.1/super-build/ckeditor.js"></script> --}}
 
